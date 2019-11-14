76dc109b238a743298ec02d763c622560cdc16a3
Kelvin Huang <huang.kelvin1@gmail.com>
2019-11-13 23:36:50 -0800

    first commit- holds example routing and rendering in js

---
+++ home.js
@@ -0,0 +1,21 @@
+let express = require('express');
+let router = express.Router();
+let path = require('path');
+let app = express();
+
+
+// GET: / and /home 
+router.get(['/', '/home'], function(req, res) {
+	res.sendFile(path.join(__dirname+'/views/home.html'));
+});
+
+
+//middlewares
+app.use('/', router);
+app.use('/', express.static(__dirname+'/css'));
+app.use('/', express.static(__dirname+'/javascript'));
+
+
+app.listen(process.env.port || 3000);
+
+console.log('Server is listening @ port 3000');
