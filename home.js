87049c6a307da3ea7bb5b217fb3d1909aa521a59
Brandon Gastelo <bagastelo@cpp.edu>
2019-11-19 15:01:20 -0800

    Create about page and footer navigation

--- home.js
+++ home.js
@@ -6,15 +6,17 @@ let app = express();
 
 // GET: / and /home
 router.get(['/', '/home'], function(req, res) {
-	res.sendFile(path.join(__dirname+'/views/home.html'));
+	  res.sendFile(path.join(__dirname+'/views/home.html'));
 });
-
+router.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname+'/views/about.html'));
+});
 
 //middlewares
 app.use('/', router);
 app.use('/', express.static(__dirname+'/css'));
 app.use('/', express.static(__dirname+'/javascript'));
-app.use('/', express.static(__dirname+'/images'));
+app.use('/images', express.static(__dirname+'/images'));
 
 app.listen(process.env.port || 3000);
 
